996132201d44a2695764092bddba445915a171a0
wishwajith13 <151111080+wishwajith13@users.noreply.github.com>
2024-06-10 10:05:46 +0530

    changing item

--- cypress/e2e/HomePageTest.cy.js
+++ cypress/e2e/HomePageTest.cy.js
@@ -7,11 +7,5 @@ describe('Home Page Test', () => {
     cy.wait(1000)
     cy.contains(/Order Your Food Here/i).should('be.visible')
   })
-  // it.only('check mouse hover',()=>{
-  //   cy.wait(3000)
-  //   // cy.getDataTest('header-animate').within(()=>{
-  //   //   cy.getDataTest('button-test').trigger('mouseover')
-  //   // })
-  //   cy.getDataTest('header-animate').trigger('mouseover')
-  // })
+  
 })

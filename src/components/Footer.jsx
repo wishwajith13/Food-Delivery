869c77da33426e424611e080d97cefb6193c4625
wishwajith13 <151111080+wishwajith13@users.noreply.github.com>
2024-07-09 16:53:21 +0530

    footer finished

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,35 +3,35 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div className='text-white bg-zinc-700 flex flex-col items-center gap-5 p-5 pt-20' id='footer'>
-        <div className=''>
-            <div>
+    <div className='text-slate-400 bg-zinc-700 flex flex-col items-center gap-5 px-32 pb-5 pt-20 mt-24' id='footer'>
+        <div className='w-full grid grid-cols-3 gap-20'>
+            <div className='flex flex-col items-start gap-5'>
                 <img src={assets.logo} alt=""/>
                 <p>Modern education empowers students, encouraging creativity, critical thinking, and collaboration, shaping tomorrow's global leaders.Preserving traditions enriches communities, fostering unity and understanding, bridging past and present for future generations.</p>
-                <div>
+                <div className='w-40 gap-3 grid grid-cols-3'>
                     <img src={assets.facebook_icon} alt='Facebook'/>
                     <img src={assets.twitter_icon} alt='Twitter'/>
                     <img src={assets.linkedin_icon} alt='Linkedin'/>
                 </div>
             </div>
-            <div>
-                <h2>COMPANY</h2>
+            <div className='flex flex-col items-start pl-40'>
+                <h2 className='text-white text-2xl font-bold pb-5'>COMPANY</h2>
                 <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Delivery</li>
-                    <li>Privacy Policy</li>
+                    <li className='mb-3'>Home</li>
+                    <li className='mb-3'>About Us</li>
+                    <li className='mb-3'>Delivery</li>
+                    <li className='mb-3'>Privacy Policy</li>
                 </ul>
             </div>
-            <div>
-                <h2>GET IN TOUCH</h2>
+            <div className='flex flex-col items-start gap-5 pl-48'>
+                <h2 className='text-white text-2xl font-bold pb-1'>GET IN TOUCH</h2>
                 <ul>
-                    <li>+94-7111-21885</li>
-                    <li>contact@tomato.com</li>
+                    <li className='mb-3'>+94-7111-21885</li>
+                    <li className='mb-3'>contact@tomato.com</li>
                 </ul>
             </div>
         </div>
-        <hr/>
+        <hr className='w-full h-1 my-5 bg-gray-500 border-0'/>
         <p>copyright 2024 Tomato.com - All Right Reserved.</p>
     </div>
   )

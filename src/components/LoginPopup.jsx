9f23ec6f11220cf9269bbd1216e841a1d9e6b5ac
wishwajith13 <151111080+wishwajith13@users.noreply.github.com>
2024-07-20 00:01:39 +0530

    finished login form

--- src/components/LoginPopup.jsx
+++ src/components/LoginPopup.jsx
@@ -1,17 +1,30 @@
 import React, { useState } from 'react'
 import { assets } from '../assets/assets'
+import Button from './Button'
 
 const LoginPopup = ({setShowLogin}) => {
 
-    const [currentState,setCurrentState] = useState("Sign Up")
+    const [currentState,setCurrentState] = useState("Log In")
 
   return (
-    <div>
-        <form>
-            <div>
+    <div className='fixed z-1 w-full h-full bg-gr grid'>
+        <form className='place-self-center max-w-80 text-zinc-700 bg-white flex flex-col gap-6 py-6 px-7 rounded-lg text-sm animate-fadein'>
+            <div className='flex justify-between items-center text-black font-bold text-2xl'>
                 <h2>{currentState}</h2>
-                <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt='cross icon'/>
+                <img className='w-4 cursor-pointer' onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt='cross icon'/>
             </div>
+            <div className='flex flex-col gap-5'>
+                {currentState==="Log In"?<></>:<input className='outline-none border-2 border-solid p-2.5 rounded' type='text' placeholder='Your Name' required/>}             
+                <input className='outline-none border-2 border-solid p-2.5 rounded' type='email' placeholder='Your Email' required/>
+                <input className='outline-none border-2 border-solid p-2.5 rounded' type='password' placeholder='Password' required/>
+            </div>
+            <Button value={currentState==="Sign Up"?"Create Account":"Log In"} buttonOutline={"border-2 border-orange-500 font-outfit text-lg"}/>
+            <div className='flex items-start gap-2 mt-3.5'>
+                <input className='mt-1' type='checkbox' required/>
+                <p className='text-orange-700 font-medium cursor-pointer'>By continuing, i agree to the terms of use & privacy policy.</p>
+            </div>
+            {currentState==="Log In"?<p>Create a new account?<span className='text-orange-700 font-medium cursor-pointer' onClick={()=>setCurrentState("Sign Up")}> Click Here</span></p>
+            :<p>Already have an account?<span className='text-orange-700 font-medium cursor-pointer' onClick={()=>setCurrentState("Log In")}> Login Here</span></p>}       
         </form>
     </div>
   )

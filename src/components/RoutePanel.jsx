c4a05372708e8d6c848945512c28f3d176528d8f
wishwajith13 <151111080+wishwajith13@users.noreply.github.com>
2025-02-24 16:26:37 +0530

    change7

--- src/components/RoutePanel.jsx
+++ src/components/RoutePanel.jsx
@@ -1,19 +1,29 @@
-import { createBrowserRouter,RouterProvider } from "react-router-dom"
+import { createBrowserRouter,HashRouter,RouterProvider } from "react-router-dom"
 import Cart from "../pages/Cart/Cart"
 import Home from "../pages/Home/Home"
 import PlaceOrder from "../pages/PlaceOrder/PlaceOrder"
 
 const routePanel=createBrowserRouter([
     {
+        path:"/cart",
+        element:<Cart/>
+    },
+    {
         path:"/",
         element:<Home/>
+    },
+    {
+        path:"/order",
+        element:<PlaceOrder/>
     },
 ])
 
 const RoutePanel =()=>{
     return(
         <div>
-           <RouterProvider router={routePanel}/>
+            <HashRouter>
+                <RouterProvider router={routePanel}/>
+            </HashRouter>
         </div>
     )
 }
